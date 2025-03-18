f90ce56809fa536253f3f85da4a160e824e357aa
jerome gerard <jerome.gerard02@outlook.fr>
2025-03-18 17:25:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/groupe_alternant_2025/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.groupe_alternant_2025;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button boutonCalcul;
+    private Button boutonHistorique;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        boutonCalcul = findViewById(R.id.bouton_calcul);
+        boutonHistorique = findViewById(R.id.bouton_historique);
+        boutonCalcul.setOnClickListener(bouton -> {
+            Toast.makeText(this,"Ceci est un toast",Toast.LENGTH_LONG).show();
+        });
+        boutonHistorique.setOnClickListener(view -> {
+            boutonCalcul.setText("j'ai fais une modif");
+        });
+    }
+}
