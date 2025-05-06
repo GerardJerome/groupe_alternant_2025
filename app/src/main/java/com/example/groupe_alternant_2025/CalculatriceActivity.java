1d6004887af1620f26476fb1aa0aa2c4af6b5b2e
jerome gerard <jerome.gerard02@outlook.fr>
2025-05-06 17:29:38 +0200

    yolo

--- app/src/main/java/com/example/groupe_alternant_2025/CalculatriceActivity.java
+++ app/src/main/java/com/example/groupe_alternant_2025/CalculatriceActivity.java
@@ -1,10 +1,7 @@
 package com.example.groupe_alternant_2025;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,29 +9,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private Button boutonCalcul;
-    private Button boutonHistorique;
+public class CalculatriceActivity extends AppCompatActivity {
+    private Button bouton0;
+    private Button bouton1;
+    private Button bouton2;
+    private Button bouton3;
+    private Button bouton4;
+    private Button bouton5;
+    private Button bouton6;
+    private Button bouton7;
+    private Button bouton8;
+    private Button bouton9;
+    private Button boutonMultiplier;
+    private Button boutonPlus;
+    private Button boutonMoins;
+    private Button boutonDiviser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculatrice);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        boutonCalcul = findViewById(R.id.bouton_calcul);
-        boutonHistorique = findViewById(R.id.bouton_historique);
-        boutonCalcul.setOnClickListener(bouton -> {
-            Intent intent = new Intent(this, CalculatriceActivity.class);
-            startActivity(intent);
-            Toast.makeText(this,"Ceci est un toast",Toast.LENGTH_LONG).show();
-        });
-        boutonHistorique.setOnClickListener(view -> {
-            boutonCalcul.setText("j'ai fais une modif");
-        });
+
+
     }
 }
